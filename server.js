0ff5e388fb7cfb546b28cb0be04353a3941efe4d
naveedgce <naveed.gce1980@gmail.com>
2022-08-23 16:01:56 +0530

    updated GET and POST

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const http = require("http")
-const port = 8081
-//http.createServer((request,response)=>{
-  // response.writeHead(200, {"content-Type": "text/html"});
-   // response.write("<h1> Hellow, this is my first server</h1>");
-    //response.end();
-//})
-//.listen(port, ()=>{
-//    console.log(`Node js server started on port ${port}`);
-//}); 
-const toDoList = ["complete Node Byte", "Paly Cricket"];
-http.createServer((request,response)=>{
-    const {method, url} = request;
-    if(url=="/todos"){
-        if(method=="GET"){
-            response.writeHead(200);
-            response.write(toDolist.tostring());
-        } else if(method=="POST"){
-            let body="";
-            request
-            .on("error", (err)=>{console.error(err);})
-            .on("data", (chunk)=>{body+=chunk;})
-            .on("end", ()=>{body=JSON.parse(body);})
-            console.log("data:", body)}
-        
-        } else{response.writeHead(501);}
-        
-    }
-);
+const http = require("http");
+const port = 8081;
+const toDoList = ["Complete Node Byte", "Play Cricket"];
+http
+  .createServer((req, res) => {
+    const { url, method } = req;
+
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200);
+        res.write(toDoList.toString());
+      } 
+      else if (method === "POST") {
+        let body = "";
+        Req
+          .on("error", (err) => console.error(err);)
+          .on("data", (chunk) => body += chunk;)
+          .on("end", () => body = JSON.parse(body);console.log("data: ", body););
+      } else {
+        res.writeHead(501);
+      }
+    } else {res.writeHead(404);}
+
+       res.end();
+  })
+
+  .listen(port, () => {
+    console.log(`Nodejs server started on port ${port}`);
+  });
+
